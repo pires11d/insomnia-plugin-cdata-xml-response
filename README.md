3181b6fcac3f4e755213527162823050a6b4d218
Lucas Joshua Pires <lucas.pires@ndd.com.br>
2021-05-21 14:57:37 -0300

    Initialized the repo

---
+++ README.md
@@ -0,0 +1,9 @@
+# Insomnia Plugin CDATA-XML
+
+This plugin removes the CDATA tags from server responses that returns a XML within it.
+
+### 🚀 Installation
+
+- Access the Application / Preferences menu and then select the Plugins tab, enter the plugin name `insomnia-plugin-cdata-xml-response` and click Install Plugin.
+
+- After installing the plugin click on enable
---
+++ main.js
@@ -0,0 +1,34 @@
+module.exports.responseHooks = [
+  ({ response }) => {
+    var responseBody = response.getBody().toString();
+    var textJson = getContent(responseBody);
+    var textXml = getContentAsXml(textJson);
+    var prettyXml = removeCDATA(textXml);
+    if (prettyXml) {
+      var buffer = Buffer.from(prettyXml);
+      response.setBody(buffer);
+    }
+  },
+];
+
+function getContent(text) {
+  try {
+    return (text.replace(/[<][^>]*[>]/g, ''));
+  } catch {
+    return null;
+  }
+}
+
+function getContentAsXml(text) {
+  text = text.replace("&lt;", "<").replace("&gt;", ">");
+  if (text.includes('&lt') || text.includes('&gt')) {
+    return getContentAsXml(text);
+  }
+  return text;
+}
+
+function removeCDATA(text) {
+  text = text.replace("<![CDATA[<DataResponse Version=\"1.0\">", "");
+  text = text.replace("</DataResponse>]]>", "");
+  return text;
+}
